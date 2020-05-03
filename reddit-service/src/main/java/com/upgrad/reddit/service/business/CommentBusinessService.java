e51a0359362757888b3e503261fc03b31a7e389b
Mansi <mansikamra00@gmail.com>
2020-05-03 16:25:52 +0530

    Sixth commit

--- reddit-service/src/main/java/com/upgrad/reddit/service/business/CommentBusinessService.java
+++ reddit-service/src/main/java/com/upgrad/reddit/service/business/CommentBusinessService.java
@@ -14,6 +14,8 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.TypedQuery;
+import java.time.ZonedDateTime;
+import java.util.UUID;
 
 @Service
 public class CommentBusinessService {
@@ -33,12 +35,21 @@ public class CommentBusinessService {
     public CommentEntity createComment(CommentEntity commentEntity, String authorization) throws AuthorizationFailedException {
 
         UserAuthEntity userAuthEntity = userDao.getUserAuthByAccesstoken(authorization);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "USER HAS NOT SIGNED IN");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is Signed Out. Sign in first to post a comment");
+        }
+        commentEntity.setDate(ZonedDateTime.now());
+        commentEntity.setUser(userAuthEntity.getUser());
+        commentEntity.setUuid(UUID.randomUUID().toString());
+        CommentEntity comment = commentDao.createComment(commentEntity);
+        return comment;
     }
 
     public PostEntity getPostByUuid(String Uuid) throws InvalidPostException {
-
         PostEntity postEntity = commentDao.getPostByUuid(Uuid);
-
+        return postEntity;
     }
 
 
@@ -48,6 +59,24 @@ public class CommentBusinessService {
     @Transactional(propagation = Propagation.REQUIRED)
     public CommentEntity editCommentContent(CommentEntity commentEntity, String commentId, String authorization) throws AuthorizationFailedException, CommentNotFoundException {
         UserAuthEntity userAuthEntity = userDao.getUserAuthByAccesstoken(authorization);
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "USER HAS NOT SIGNED IN");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is Signed Out. Sign in first to edit a comment");
+        }
+        CommentEntity commentToBeEdited = commentDao.getCommentByUuid(commentId);
+        if (commentToBeEdited == null) {
+            throw new CommentNotFoundException("COM-001", "Entered comment uuid does not exist");
+        }
+        if (!commentToBeEdited.getUser().getUserName().equalsIgnoreCase(userAuthEntity.getUser().getUserName())) {
+            throw new AuthorizationFailedException("ATHR-003", "Only the comment owner can edit the comment");
+        }
+        commentEntity.setUuid(UUID.randomUUID().toString());
+        commentEntity.setDate(ZonedDateTime.now());
+        commentEntity.setUser(userAuthEntity.getUser());
+        commentEntity.setPost(commentToBeEdited.getPost());
+        CommentEntity commentEdited = commentDao.editComment(commentEntity);
+        return commentEdited;
 
     }
 
@@ -57,7 +86,22 @@ public class CommentBusinessService {
     @Transactional(propagation = Propagation.REQUIRED)
     public CommentEntity deleteComment(String commentId, String authorization) throws AuthorizationFailedException, CommentNotFoundException {
         UserAuthEntity userAuthEntity = userDao.getUserAuthByAccesstoken(authorization);
-
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "USER HAS NOT SIGNED IN");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is Signed Out. Sign in first to delete a comment");
+        }
+        CommentEntity commentToBeDeleted = commentDao.getCommentByUuid(commentId);
+        if (commentToBeDeleted == null) {
+            throw new CommentNotFoundException("COM-001", "Entered commment uuid does not exit");
+        }
+        if (!userAuthEntity.getUser().getRole().equalsIgnoreCase("admin")) {
+            if(!commentToBeDeleted.getUser().getUserName().equalsIgnoreCase(userAuthEntity.getUser().getUserName())){
+                throw new AuthorizationFailedException("ATHR-003", "Only the comment owner or admin can delete the comment");
+            }
+        }
+        CommentEntity commetDeleted = commentDao.deleteComment(commentToBeDeleted);
+        return commetDeleted;
     }
 
     /**
@@ -65,6 +109,16 @@ public class CommentBusinessService {
      */
     public TypedQuery<CommentEntity> getCommentsByPost(String postId, String authorization) throws AuthorizationFailedException, InvalidPostException {
         UserAuthEntity userAuthEntity = userDao.getUserAuthByAccesstoken(authorization);
-
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "USER HAS NOT SIGNED IN");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is Signed Out. Sign in first to get the comments");
+        }
+        PostEntity post = commentDao.getPostByUuid(postId);
+        if (post == null) {
+            throw new InvalidPostException("POS-001", "The post with entered uuid whose details are to be seen does not exist");
+        }
+        TypedQuery<CommentEntity> comments = commentDao.getCommentsByPost(post);
+        return comments;
     }
 }
